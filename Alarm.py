5e49d894f1d621335ec00e92f85f58ab6916f0b5
Hamiterol <hamit_1061@hotmail.com>
2019-04-11 23:53:54 +0300

    Update Alarm.py

--- Alarm.py
+++ Alarm.py
@@ -1,11 +1,12 @@
 import time
-
+//Buraya dikkat kullanıcıdan alınan veriler her zaman string değerdedir, bu dolayıdır ki gelen verileri int çeviriyoruz
 saat=int(input("Saat:" ))
 Dakika=int(input("Dakika:" ))
 
 while True:
-    lc=time.localtime(time.time())
-    if saat==lc.tm_hour and Dakika==lc.tm_min:
+    
+    lc=time.localtime(time.time())#burada bilgisayarın saat ve zamanının alıyoruz
+    if saat==lc.tm_hour and Dakika==lc.tm_min:#kullanıcıdan alınan deger ile pc deki degerleri kontrol ettiriyoruz
         print ("Saat Çaldı")
         break
     else:
